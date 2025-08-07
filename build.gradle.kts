a45981945dfc47986c71fa3c2a9401351b745eef
Shakzz890 <120597222+Shakzz890@users.noreply.github.com>
2025-08-07 15:42:55 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,7 +38,7 @@ subprojects {
 
     cloudstream {
         // when running through github workflow, GITHUB_REPOSITORY should contain current repository name
-        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/Shakzz890/Shakzz890")
+        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/Shakzz890/ShakzzCutie")
 
         authors = listOf("megix")
     }
